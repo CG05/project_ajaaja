9ce4642dff5e9dc03bf3fc639fe18939cd023d9a
dayeon <tlsekdus77@naver.com>
2024-05-27 16:11:27 +0900

    다연

---
+++ main/main/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include, re_path
+from . import views
+from . import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", views.index),
+    path("account/login/", views.login, name='login'),
+    path("account/logout/", views.logout, name='logout'),
+    path("account/register/", views.createAccount, name="create"),
+]
---
+++ main/main/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib.auth.hashers import check_password
+def createAccount(request):
+    if request.method == 'GET':
+        return render(request, "registration/register.html")
+    elif request.method == 'POST':
+        username = request.POST.get("username");
+        password = request.POST.get("password");
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+
+        try:
+            User.objects.create_user(username, email, password, 
+            first_name=first_name, last_name=last_name)
+
+            return redirect('login')
+        except:
+            msg = "<script>";
+            msg += "alert('같은 아이디가 존재합니다. 다시 가입하세요.');";
+            msg += "location.href='/account/register';";
+            msg += "</script>";
+            return HttpResponse(msg);
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'registration/login.html');
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(request, username=username, password=password);
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            msg = "<script>";
+            msg += "alert('로그인 아이디/비밀번호가 틀립니다. 다시 로그인 하세요.');";
+            msg += "location.href='/account/login';";
+            msg += "</script>";
+            return HttpResponse(msg);
+
+def logout(request):
+    auth.logout(request);
+    return render(request, "registration/logged_out.html");
+
+def index(request):
+    return render(request, "main.html");
