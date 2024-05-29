14946eefc9e60ec6f80c1dee353eef08e56e98ee
CG05 <sungcjfrb2@gmail.com>
2024-05-29 17:00:29 +0900

    페이지 로딩 완료

--- main/page/views.py
+++ main/page/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Page
+import json 
 
 def default(request):
     return render(request, "page/default.html")
 
 def getpage(request, pageid):
     page = Page.objects.get(id=pageid)
+    
     return render(request, "page/page.html", {"page": page})
