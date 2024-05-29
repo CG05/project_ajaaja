591427c2f52521fa2160e77b04ee838853f4d0ac
CG05 <sungcjfrb2@gmail.com>
2024-05-29 10:00:49 +0900

    model

--- main/page/admin.py
+++ main/page/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Page
 
 # Register your models here.
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('username', 'pagepath', 'content', 'title', 'created_at', 'updated_at')
+
+
+admin.site.register(Page, PageAdmin)
