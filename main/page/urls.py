7bd1d319ed4833c4f6de747d641627c657a246d3
CG05 <sungcjfrb2@gmail.com>
2024-05-30 13:49:38 +0900

    페이지 저장 진행중

--- main/page/urls.py
+++ main/page/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.default),
     # re_path(r'^items/(?P<ids>(\d+(_\d+)*))/$', views.getpage),
     path("<pageid>/", views.getpage),
+    path("<pageid>/save/", views.savepage),
 ]
--- main/page/views.py
+++ main/page/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Page
-import json 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 def default(request):
     return render(request, "page/default.html")
@@ -8,4 +10,25 @@ def default(request):
 def getpage(request, pageid):
     page = Page.objects.get(id=pageid)
     
-    return render(request, "page/page.html", {"page": page})
+    return render(request, "page/page.html", {"pageid": pageid, "page": page})
+
+@csrf_exempt
+def savepage(request, pageid):
+    if request.method == 'POST':
+        try:
+            page = Page.objects.get(id=pageid)
+
+            data = json.loads(request.body)
+            content = data.get('content')
+            title = content.get('title')
+            contents = json.loads(content.get('contents'));
+
+            page.title = title
+            page.content = contents
+            page.save()
+
+            return JsonResponse({'message': 'Data saved successfully!'})
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
